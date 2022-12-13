2171b29c555b4555bfa6fcc843834dd71fad3f8d
endlmk <1269467+endlmk@users.noreply.github.com>
2022-12-13 21:57:49 +0900

    Impl one digit.

--- LCD/LcdNumbers.cs
+++ LCD/LcdNumbers.cs
@@ -2,13 +2,52 @@
 {
     public class LcdNumbers
     {
-        const string one = "  \n" +
-                           " |\n" +
-                           " |";
+        private const string Zero = " _ \n" +
+                                    "| |\n" +
+                                    "|_|";
+
+        private const string One = "  \n" +
+                                   " |\n" +
+                                   " |";
+
+        private const string Two = " _ \n" +
+                                   " _|\n" +
+                                   "|_ ";
+
+        private const string Three = " _ \n" +
+                                     " _|\n" +
+                                     " _|";
+
+        private const string Four = "   \n" +
+                                    "|_|\n" +
+                                    "  |";
+
+
+        private const string Five = " _ \n" +
+                                    "|_ \n" +
+                                    " _|";
+
+        private const string Six = " _ \n" +
+                                   "|_ \n" +
+                                   "|_|";
+
+        private const string Seven = " _ \n" +
+                                     "  |\n" +
+                                     "  |";
+
+        private const string Eight = " _ \n" +
+                                     "|_|\n" +
+                                     "|_|";
+
+        private const string Nine = " _ \n" +
+                                    "|_|\n" +
+                                    " _|";
+
+        private static readonly string[] Numbers = new string[] { Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine };
 
         public static string Show(int number)
         {
-            return one;
+            return Numbers[number];
         }
     }
 }
--- TDD-lcd-cs/LCDTest.cs
+++ TDD-lcd-cs/LCDTest.cs
@@ -12,6 +12,60 @@ namespace TDD_lcd_cs
                                " |";
 
             Assert.Equal(one, LcdNumbers.Show(1));
+
+            const string two = " _ \n" +
+                               " _|\n" +
+                               "|_ ";
+
+            Assert.Equal(two, LcdNumbers.Show(2));
+
+            const string three = " _ \n" +
+                                 " _|\n" +
+                                 " _|";
+
+            Assert.Equal(three, LcdNumbers.Show(3));
+
+            const string four = "   \n" +
+                                "|_|\n" +
+                                "  |";
+
+            Assert.Equal(four, LcdNumbers.Show(4));
+
+            const string five = " _ \n" +
+                                "|_ \n" +
+                                " _|";
+
+            Assert.Equal(five, LcdNumbers.Show(5));
+
+            const string six = " _ \n" +
+                               "|_ \n" +
+                               "|_|";
+
+            Assert.Equal(six, LcdNumbers.Show(6));
+
+            const string seven = " _ \n" +
+                                 "  |\n" +
+                                 "  |";
+
+            Assert.Equal(seven, LcdNumbers.Show(7));
+
+            const string eight = " _ \n" +
+                                 "|_|\n" +
+                                 "|_|";
+
+            Assert.Equal(eight, LcdNumbers.Show(8));
+
+            const string nine = " _ \n" +
+                                "|_|\n" +
+                                " _|";
+
+            Assert.Equal(nine, LcdNumbers.Show(9));
+
+            const string zero = " _ \n" +
+                                "| |\n" +
+                                "|_|";
+
+            Assert.Equal(zero, LcdNumbers.Show(0));
         }
     }
 }
